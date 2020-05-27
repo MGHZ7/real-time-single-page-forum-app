9b2f58c3da82c43e8b4b18bff0eac5fe08b36f65
Motasem Gozlan <motasem.go.26@gmail.com>
2020-05-27 19:50:43 +0300

    CRUD for Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryResource;
 use App\Model\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -14,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return CategoryResource::collection(Category::latest()->get());
     }
 
     /**
@@ -30,29 +33,40 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->save();
+
+        return response()->json(
+            [
+                'message' => 'created',
+                'code' => 202
+            ],
+            Response::HTTP_ACCEPTED
+        );
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Category  $category
-     * @return \Illuminate\Http\Response
+     * @param \App\Model\Category $category
+     * @return \App\Http\Resources\CategoryResource
      */
     public function show(Category $category)
     {
-        //
+        return new \App\Http\Resources\CategoryResource($category);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Category  $category
+     * @param \App\Model\Category $category
      * @return \Illuminate\Http\Response
      */
     public function edit(Category $category)
@@ -63,23 +77,42 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Category  $category
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Model\Category $category
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Category $category)
     {
-        //
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name, '-')
+        ]);
+
+        return \response()->json(
+            [
+                'message' => 'Category Updated Successfully',
+                'code' => Response::HTTP_ACCEPTED
+            ],
+            202
+        );
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Category  $category
+     * @param \App\Model\Category $category
      * @return \Illuminate\Http\Response
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return \response()->json(
+            [
+                'message' => 'Category Deleted Successfully',
+                'code' => 202
+            ],
+            Response::HTTP_ACCEPTED
+        );
     }
 }
--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -81,7 +81,15 @@ class QuestionController extends Controller
      */
     public function update(Request $request, Question $question)
     {
-        //
+        $question->update($request->all());
+
+        return \response()->json(
+          [
+              'message' => "Resource Updated Successfully",
+              'code' => 202
+          ],
+            Response::HTTP_ACCEPTED
+        );
     }
 
     /**
