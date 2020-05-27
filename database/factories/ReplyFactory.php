1be087b6bf8bab0a6489d89e83482ce44a1a7280
Motasem Gozlan <motasem.go.26@gmail.com>
2020-05-27 11:42:07 +0300

    Factories and seeds

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -4,9 +4,14 @@
 
 use App\Model\Reply;
 use Faker\Generator as Faker;
-
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text,
+        'question_id' => function(){
+            return \App\Model\Question::all()->random();
+        },
+        'user_id' => function(){
+            return \App\User::all()->random();
+        }
     ];
 });
