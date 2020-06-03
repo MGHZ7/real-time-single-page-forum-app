780f48b32be9b274f5525c3087778622b34bfdbd
Motasem Gozlan <motasem.go.26@gmail.com>
2020-06-03 23:29:59 +0300

    Veutify and VueRouter initialization

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <toolbar></toolbar>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+    import Toolbar from "./Toolbar";
+    import AppFooter from "./AppFooter";
+    export default {
+        components: {AppFooter, Toolbar}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-card class="overflow-hidden">
+        <v-app-bar
+            absolute
+            color="indigo darken-2"
+            dark
+            shrink-on-scroll
+            prominent
+            scroll-target="#scrolling-techniques"
+        >
+            <v-btn icon>
+                <v-icon>mdi-menu</v-icon>
+            </v-btn>
+
+            <v-toolbar-title>MGHZ</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn text>
+                    Forum
+                </v-btn>
+                <v-btn text>
+                    Ask Question
+                </v-btn>
+                <v-btn text>Category</v-btn>
+                <v-btn text>Login</v-btn>
+            </v-toolbar-items>
+
+        </v-app-bar>
+        <v-sheet
+            id="scrolling-techniques"
+            class="overflow-y-auto"
+            max-height="600"
+        >
+            <v-container style="height: 1000px;"></v-container>
+        </v-sheet>
+    </v-card>
+</template>
+
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
